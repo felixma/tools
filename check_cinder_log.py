2e2358f25c925f35475075e2763215b12c3cbca7
felix23ma <felix23ma@gmail.com>
2016-02-04 16:52:03 +0800

    use print_usage()

--- check_cinder_log.py
+++ check_cinder_log.py
@@ -46,30 +46,36 @@ def check_cinder_scheduler_log(word):
         os.system("ssh %s grep %s /var/log/cinder/scheduler.log" % (svr, word))
     print
 
-usage = 'usage: %prog [options] keyword'
-parser = optparse.OptionParser(usage=usage)
-parser.add_option("-a", "--api", action='store_true',
-                  default=False, dest='api',
-                  help="check cinder-api log")
-parser.add_option("-v", "--volume", action='store_true',
-                  default=False, dest='volume',
-                  help="check cinder-volume log")
-parser.add_option("-b", "--backup", action='store_true',
-                  default=False, dest='backup',
-                  help="check cinder-backup log")
-parser.add_option("-s", "--scheduler", action='store_true',
-                  default=False, dest='scheduler',
-                  help="check cinder-scheduler log")
 
-(options, remainder) = parser.parse_args()
+def main():
+    usage = 'usage: %prog [options] keyword'
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-a", "--api", action='store_true',
+                      default=False, dest='api',
+                      help="check cinder-api log")
+    parser.add_option("-v", "--volume", action='store_true',
+                      default=False, dest='volume',
+                      help="check cinder-volume log")
+    parser.add_option("-b", "--backup", action='store_true',
+                      default=False, dest='backup',
+                      help="check cinder-backup log")
+    parser.add_option("-s", "--scheduler", action='store_true',
+                      default=False, dest='scheduler',
+                      help="check cinder-scheduler log")
 
-if not remainder:
-    print "No keyword provided. Run 'check_cinder_log.py -h' to show usage."
-if options.api:
-    check_cinder_api_log(remainder[0])
-if options.volume:
-    check_cinder_volume_log(remainder[0])
-if options.backup:
-    check_cinder_backup_log(remainder[0])
-if options.scheduler:
-    check_cinder_scheduler_log(remainder[0])
+    (options, remainder) = parser.parse_args()
+
+    if not remainder:
+        parser.print_usage()
+        return
+    if options.api:
+        check_cinder_api_log(remainder[0])
+    if options.volume:
+        check_cinder_volume_log(remainder[0])
+    if options.backup:
+        check_cinder_backup_log(remainder[0])
+    if options.scheduler:
+        check_cinder_scheduler_log(remainder[0])
+
+if __name__ == '__main__':
+    main()
